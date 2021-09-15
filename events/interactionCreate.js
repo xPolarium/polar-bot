020565a6344cab6e5261015c70712ed18072945c
Miguel Torres <miguel@Miguels-MacBook-Pro.local>
2021-09-15 11:47:59 -0500

    finished interactionCreate functions

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,22 @@
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+	name: "interactionCreate",
+	async execute(interaction) {
+		if (!interaction.isCommand()) return;
+
+		const { client, commandName } = interaction;
+
+		const command = client.commands.get(commandName);
+
+		if (!command) return;
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({
+				content: "There was an error while executing this command!",
+				ephemeral: true,
+			});
+		}
 	},
 };
